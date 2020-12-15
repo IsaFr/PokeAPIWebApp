1807e1d54944aa95a046e35360ef2efabbec60a8
Isamar Francisco <isamarfrancisco26@gmail.com>
2020-12-14 20:03:03 -0400

    Add project files.

---
+++ Data/FileSaver.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeAPIWebApp.Data
+{
+    public class FileSaver : IFileSaver
+    {
+        private IJSRuntime _oIJSRuntime { get;  }
+
+        public FileSaver(IJSRuntime oIJSRuntime)
+        {
+            _oIJSRuntime = oIJSRuntime;
+        }
+        public async Task SaveAs(string fileName, List<string> Data, string type = "text.plain;charset=utf-8")
+        {
+            await _oIJSRuntime.InvokeVoidAsync("FileSaveBlazor.saveAs", fileName, Data, type);
+        }
+    }
+}
---
+++ Data/IFileSaver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeAPIWebApp.Data
+{
+    public interface IFileSaver
+    {
+        Task SaveAs(string fileName, List<string> Data, string type = "text.plain;charset=utf-8");
+    }
+}
